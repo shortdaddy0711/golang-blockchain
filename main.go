1421e6d85b1ffb17d02c7e8fc1ae61f8d2b7a1c6
shortdaddy0711 <sodoatm@gmail.com>
2021-01-04 12:17:03 -0800

    generate dockerfile to build docker image

--- main.go
+++ main.go
@@ -17,10 +17,10 @@ type CommandLine struct {
 
 func (cli *CommandLine) printUsage() {
 	fmt.Println("Usage:")
-	fmt.Println(" getbalance -address ADDRESS - get the balance for an address")
-	fmt.Println(" createblockchain -address ADDRESS creates a blockchain and sends genesis reward to address")
+	fmt.Println(" getbalance -address ADDRESS - Get the balance for an address")
+	fmt.Println(" createblockchain -address ADDRESS - Creates a blockchain and sends genesis reward to address")
 	fmt.Println(" printchain - Prints the blocks in the chain")
-	fmt.Println(" send -from FROM -to TO -amount AMOUNT -Send amount of coins")
+	fmt.Println(" send -from FROM -to TO -amount AMOUNT - Send amount of coins")
 
 }
 
